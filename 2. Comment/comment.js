d638f6f8cd84f8c75e1dab6a46ed8fa1597f1441
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-18 14:30:23 +0700

    Comment

---
+++ 2. Comment/comment.js
@@ -0,0 +1,16 @@
+// this is a comment
+
+/* this is a comment for multiple line */
+
+// console.log("Hello World"); (this code will not execute)
+
+/*
+
+    this code will not execute
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+
+*/
