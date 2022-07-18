9376f40cd00dd6a67c2d96f758316770e3065090
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-18 14:41:49 +0700

    Numbers Data Type

---
+++ 3. Number Data Type/number.js
@@ -0,0 +1,18 @@
+// integer number
+console.log(100);
+
+// floating point number
+console.log(100.5);
+
+// number notation
+// hexadecimal: add 0x before the number
+console.log(0xff);
+
+// binary: add 0b before the number
+console.log(0b1010);
+
+// octal: add 0o before the number
+console.log(0o744);
+
+// exponential notation
+console.log(1e6);
