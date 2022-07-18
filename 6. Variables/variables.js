90b5f19751559009c1cd7d3f450a136748b5702f
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-18 15:32:56 +0700

    Variables

---
+++ 6. Variables/variables.js
@@ -0,0 +1,31 @@
+// variable using the var keyword
+
+// initialize variable
+var firstName;
+myName = "John";
+
+// we can reassign the value of a variable
+myName = "Jane";
+
+// initialize variable with value
+var lastName = "Iskandar";
+console.log(firstName + " " + lastName);
+
+// variable using the let keyword
+// initialize variable
+let firstName = "John";
+let lastName = "Iskandar";
+
+// we can reassign the value of a variable
+firstName = "Jane";
+
+console.log(firstName + " " + lastName);
+
+// variable using the const keyword
+// initialize variable
+const firstName = "John";
+const lastName = "Iskandar";
+
+// for const variable, we can't reassign the value
+firstName = "Jane"; // error
+console.log(firstName + " " + lastName);
