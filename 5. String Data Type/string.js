90bd766268c154b364aeacfecac703b04b312708
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-18 15:04:08 +0700

    String Data Type

---
+++ 5. String Data Type/string.js
@@ -0,0 +1,40 @@
+// string using ""
+console.log("Hello World");
+// string using ''
+console.log("Hello World");
+// string using ``
+console.log(`Hello World`);
+
+// string concatenation
+console.log("Name: " + "Daffa Haidar Nabil Zufar");
+console.log(
+  "Name: " + "Daffa" + " " + "Haidar" + " " + "Nabil" + " " + "Zufar"
+);
+
+// escape character
+// \n new line
+console.log("Hello\nWorld");
+// \t tab
+console.log("Hello\tWorld");
+// \b backspace
+console.log("Hello\bWorld");
+// \r carriage return
+console.log("Hello\rWorld");
+// \f form feed
+console.log("Hello\fWorld");
+// \v vertical tab
+console.log("Hello\vWorld");
+// \0 null
+console.log("Hello\0World");
+// \ddd octal
+console.log("Hello\077World");
+// \xdd hexadecimal
+console.log("Hello\x41World");
+// \uddd unicode
+console.log("Hello\u0041World");
+// \' single quote
+console.log("Hello'World'");
+// \" double quote
+console.log('Hello"World"');
+// \\ backslash
+console.log("Hello\\World\\");
