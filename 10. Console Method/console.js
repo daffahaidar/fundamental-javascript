458f92e0c6e61bfbf0f3b1bc7988e4af25954ddf
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-19 16:50:46 +0700

    Console Method

---
+++ 10. Console Method/console.js
@@ -0,0 +1,12 @@
+// print debug info to console
+console.debug("Hello World!");
+
+// print info to console
+console.log("Hello World!");
+console.info("Hello World!");
+
+// print warning to console
+console.warn("Hello World!");
+
+// print error to console
+console.error("Hello World!");
