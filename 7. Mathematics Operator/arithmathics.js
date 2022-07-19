8ff14c092a2ad5476b63c4294a0802dd14a8c0a6
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-19 16:13:55 +0700

    Mathematics Operator

---
+++ 7. Mathematics Operator/arithmathics.js
@@ -0,0 +1,31 @@
+let number1 = 10;
+let number2 = 20;
+
+// addition
+let result = number1 + number2;
+console.log(result);
+
+// subtraction
+result = number2 - number1;
+console.log(result);
+
+// multiplication
+result = number1 * number2;
+console.log(result);
+
+// division
+result = number2 / number1;
+console.log(result);
+
+// modulus
+result = number2 % number1;
+console.log(result);
+
+// exponentiation
+result = result + 1;
+console.log(result);
+
+// or
+
+result += 1;
+console.log(result);
---
+++ 7. Mathematics Operator/unary.js
@@ -0,0 +1,13 @@
+// unary operator
+let a = 10; //plus
+let b = -20; //minus
+
+// increment
+console.log(a++);
+/* or */
+console.log(++a);
+
+// decrement
+console.log(a--);
+/* or */
+console.log(--a);
