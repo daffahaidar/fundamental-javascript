cf21e78dc07eafc10b1320b7642538f6bf367cfc
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-19 16:27:55 +0700

    Comparations

---
+++ 8. Comparation/comparation.js
@@ -0,0 +1,30 @@
+let a = 2;
+let b = 3;
+let result;
+
+// comparison
+result = a > b;
+console.log(result);
+
+result = a < b;
+console.log(result);
+
+result = a >= b;
+console.log(result);
+
+result = a <= b;
+console.log(result);
+
+// comparation with different data types
+result = 9 == "9";
+console.log(result);
+
+result = 9 != "9";
+console.log(result);
+
+// comparation with same data types
+result = 9 === "9";
+console.log(result);
+
+result = 9 !== "9";
+console.log(result);
