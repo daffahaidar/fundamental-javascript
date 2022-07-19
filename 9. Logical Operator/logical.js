235ed56f0c76714418f9a572ac5f040a7b8a9042
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-19 16:40:02 +0700

    Logical Operator

---
+++ 9. Logical Operator/logical.js
@@ -0,0 +1,20 @@
+// logical operator
+let a = true;
+let b = false;
+let result;
+
+// logical AND (will return true if both operands are true)
+result = a && b;
+console.log(result);
+
+// logical OR (will return true if one of the operands is true)
+result = a || b;
+console.log(result);
+
+// logical NOT (will return false if the operand is true)
+result = !a;
+console.log(result);
+
+// logical XOR (will return true if only one of the operands is true)
+result = a ^ b;
+console.log(result);
