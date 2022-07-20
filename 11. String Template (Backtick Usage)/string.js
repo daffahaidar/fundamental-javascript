73a3afd5b378414b7b8a5db9b1f6f933d06c75c5
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-20 19:16:18 +0700

    String Template

---
+++ 11. String Template (Backtick Usage)/string.js
@@ -0,0 +1,19 @@
+const firstName = "Daffa Haidar";
+const middleName = "Nabil";
+const lastName = "Zufar";
+
+// using string template
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+// without string template
+console.log(firstName + " " + middleName + " " + lastName);
+
+// string template also can be used to concatenate multiple string
+console.log(`Line 1
+Line 2
+Line 3
+Line 4
+Line 5`);
+
+// without string template we can't use new line (must use \n)
+console.log("Line 1\nLine 2\nLine 3\nLine 4\nLine 5");
