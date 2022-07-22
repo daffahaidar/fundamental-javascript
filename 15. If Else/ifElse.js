dbb7dbe82d76d3e3540a7bf3199a1df1b570c35d
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-22 14:22:50 +0700

    If Else

---
+++ 15. If Else/ifElse.js
@@ -0,0 +1,13 @@
+let grade = "A";
+
+if (grade === "A") {
+  console.log("You are excellent");
+} else if (grade === "B") {
+  console.log("You are good");
+} else if (grade === "C") {
+  console.log("You are average");
+} else if (grade === "D") {
+  console.log("You are bad");
+} else {
+  console.log("You are very bad");
+}
