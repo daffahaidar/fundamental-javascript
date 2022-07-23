8a4c3d9e2aadc319140e5ecdb3fc47a77c41d019
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-23 19:36:55 +0700

    Switch Case

---
+++ 19.  Switch Case/switch.js
@@ -0,0 +1,16 @@
+let examValue = 90;
+
+switch (examValue) {
+  case 90:
+    console.log("Grade A");
+    break;
+  case 80:
+    console.log("Grade B");
+    break;
+  case 70:
+    console.log("Grade C");
+    break;
+  case 60:
+    console.log("Grade D");
+    break;
+}
