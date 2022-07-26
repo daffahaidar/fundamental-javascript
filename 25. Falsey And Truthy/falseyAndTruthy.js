5d6eac0fab2b14d96b0e864010b0383f7f572a02
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-26 17:20:36 +0700

    Falsey and Truthy

---
+++ 25. Falsey And Truthy/falseyAndTruthy.js
@@ -0,0 +1,34 @@
+// falsey value in javascript is:
+/*
+ * false
+ * 0
+ * ''
+ * null
+ * undefined
+ * NaN
+ * ""
+ */
+
+// Truthy value in javascript is:
+/*
+ * true
+ * 1
+ * 'a'
+ * []
+ * {}
+ * function(){}
+ */
+
+// example
+let a = 0;
+let b = "Daffa Haidar";
+
+if (a) {
+  console.log("a is truthy");
+} else {
+  console.log("a is falsey");
+}
+
+if (b) {
+  console.log("b is truthy");
+}
