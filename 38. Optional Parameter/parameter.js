44fa0a65033bfda5e88355257df7082c3421d227
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-07-28 14:35:39 +0700

    Optional Parameter

---
+++ 38. Optional Parameter/parameter.js
@@ -0,0 +1,8 @@
+function myName(firstName, middleName, lastName) {
+  console.log(firstName + " " + middleName + " " + lastName);
+}
+
+myName("John", "Paul", "Jones");
+
+// optional parameter
+myName("Daffa", "Haidar"); // output: Daffa Haidar undefined
